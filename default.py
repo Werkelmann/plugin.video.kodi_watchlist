067fa24879cf6458f70395668c5c924edb5b74e2
Werkelmann <al-kern@web.de>
2017-01-12 19:23:30 +0100

    added settings

--- default.py
+++ default.py
@@ -15,11 +15,13 @@ def get_key_path():
 addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'movies')
 
-key = open(get_key_path()).read()
+apiKey = open(get_key_path()).read()
+user = xbmcplugin.getSetting(addon_handle, 'username')
+password = xbmcplugin.getSetting(addon_handle, 'password')
 
 # following from example:
 url = 'http://localhost/some_video.mkv'
-li = xbmcgui.ListItem(key, iconImage='DefaultVideo.png')
+li = xbmcgui.ListItem(user, iconImage='DefaultVideo.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
