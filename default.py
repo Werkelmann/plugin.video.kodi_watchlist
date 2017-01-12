b1e8d2038e31f7640554956dc188b134d825ed5f
Werkelmann <al-kern@web.de>
2017-01-12 19:57:56 +0100

    added api to get authentification token

--- default.py
+++ default.py
@@ -1,4 +1,6 @@
 import sys
+import json
+import urllib2
 import xbmc
 import xbmcgui
 import xbmcplugin
@@ -16,12 +18,35 @@ addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'movies')
 
 apiKey = open(get_key_path()).read()
+
+
+def get_api_root():
+    return 'https://api.themoviedb.org/3/'
+
+
+def add_api_key():
+    return '?api_key=' + apiKey
+
+
+def build_api_url(resource):
+    return get_api_root() + resource + add_api_key()
+
+
+def get_authentification_token():
+    token_URL = build_api_url('authentication/token/new')
+    http_response = urllib2.urlopen(token_URL)
+    json_response = http_response.read().decode('utf-8')
+    response = json.loads(json_response)
+    return response['request_token']
+
+
 user = xbmcplugin.getSetting(addon_handle, 'username')
 password = xbmcplugin.getSetting(addon_handle, 'password')
 
+
 # following from example:
 url = 'http://localhost/some_video.mkv'
-li = xbmcgui.ListItem(user, iconImage='DefaultVideo.png')
+li = xbmcgui.ListItem(get_authentification_token(), iconImage='DefaultVideo.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
