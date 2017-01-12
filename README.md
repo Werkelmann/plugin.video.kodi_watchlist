21d188f438c316d1a8a76754189139111d8c6a8c
Werkelmann <al-kern@web.de>
2017-01-12 19:04:45 +0100

    added key reading

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # plugin.video.kodi_watchlist
+
+1. Put MovieDB-API-Key in key.txt file in this directory.
--- default.py
+++ default.py
@@ -1,13 +1,25 @@
 import sys
+import xbmc
 import xbmcgui
 import xbmcplugin
 
-addon_handle = int(sys.argv[1])
 
+def get_key_path():
+    root_path = xbmc.translatePath('special://home')
+    if xbmc.getCondVisibility('system.platform.windows'):
+        return root_path + 'addons\\plugin.video.kodi_watchlist\\key.txt'
+    if xbmc.getCondVisibility('system.platform.linux'):
+        return root_path + 'addons/plugin.video.kodi_watchlist/key.txt'  # TODO linux support is untested
+
+
+addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'movies')
 
+key = open(get_key_path()).read()
+
+# following from example:
 url = 'http://localhost/some_video.mkv'
-li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
+li = xbmcgui.ListItem(key, iconImage='DefaultVideo.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
